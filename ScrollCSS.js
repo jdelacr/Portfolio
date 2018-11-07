132dc4b4ba76e032f94962e7d2e2702e09605251
Jan Justyn Dela Cruz <justyn@Jans-MacBook-Pro.local>
2018-11-07 11:27:35 -0500

    Finished implementing the Scroll event CSS

--- ScrollCSS.js
+++ ScrollCSS.js
@@ -1,12 +1,34 @@
-window.onscroll = function () {scrollEvents();};
+window.onscroll = function () {scrollEvents(); scrollEvents2(); scrollEvents3();};
 
 function scrollEvents() {
     if (document.body.scrollTop > 70 || document.documentElement.scrollTop > 70){
         document.getElementById("Bio").className += " slideIn1";
+        document.getElementById("Bio_Content").className += " slideIn2";
     }
     else {
-        document.getElementById("Bio"). className = "parallelogram";
+        document.getElementById("Bio").className = "parallelogram";
+        document.getElementById("Bio_Content").className = "content1";
     }
+}
 
+function scrollEvents2() {
+    if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000){
+        document.getElementById("Academics").className += " slideIn1";
+        document.getElementById("Academic_Content").className += " slideIn2";
+    }
+    else {
+        document.getElementById("Academics").className = "parallelogram";
+        document.getElementById("Academic_Content").className = "content1";
+    }
+}
 
+function scrollEvents3() {
+    if (document.body.scrollTop > 2000 || document.documentElement.scrollTop > 2000){
+        document.getElementById("Skills").className += " slideIn1";
+        document.getElementById("Skill_Content").className += " slideIn2";
+    }
+    else {
+        document.getElementById("Skills").className = "parallelogram";
+        document.getElementById("Skill_Content").className = "content1";
+    }
 }
